c4592a6dc367c7a3c632096805611d83537eca98
Unknown <daichizaki@yahoo.co.jp>
2018-03-28 00:23:50 +0900

    add ip

--- speach.js
+++ speach.js
@@ -1,8 +1,7 @@
-const googlehome = require('google-home-notifier')
+const googlehome = require('google-home-notifier');
 const language = 'ja';
-
-googlehome.device('Google-Home', language);
-
+googlehome.device("Google-Home", language);
+googlehome.ip("192.168.179.3");
 googlehome.notify('こんにちは。私はグーグルホームです。', function(res) {
   console.log(res);
 });
